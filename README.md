a2be71a6f43ff41fdafcab77f1ccf9751b62030c
brunojensen <brunojensen>
2013-02-14 11:04:40 -0200

    dead repo

--- README.md
+++
@@ -1,4 +0,0 @@
-br.com.bea.androidtools
-=======================
-
-android-tools
--- src/main/java/br/com/bea/androidtools/api/model/Entity.java
+++
@@ -1,8 +0,0 @@
-package br.com.bea.androidtools.api.model;
-
-public abstract class Entity<Id> extends ValueObject {
-
-    public abstract Id getId();
-
-    public abstract void setId(final Id id);
-}

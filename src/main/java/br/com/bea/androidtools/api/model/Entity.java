44881fd90dfe754b96a673419115ee1c4c948faf
@brunojensen <@brunojensen>
2013-02-18 22:45:17 -0300

    Sincronização do framework

--- src/main/java/br/com/bea/androidtools/api/model/Entity.java
+++ src/main/java/br/com/bea/androidtools/api/model/Entity.java
@@ -2,6 +2,8 @@ package br.com.bea.androidtools.api.model;
 
 public abstract class Entity<Id> extends ValueObject {
 
+    private static final long serialVersionUID = 1L;
+
     public abstract Id getId();
 
     public abstract void setId(final Id id);
--- src/main/java/br/com/bea/androidtools/api/model/ValueObject.java
+++ src/main/java/br/com/bea/androidtools/api/model/ValueObject.java
@@ -1,12 +1,29 @@
 package br.com.bea.androidtools.api.model;
 
+import java.io.Serializable;
 import java.util.Date;
+import android.os.Parcel;
 import android.os.Parcelable;
 
-public abstract class ValueObject implements Parcelable {
+public abstract class ValueObject implements Parcelable, Serializable {
+    private static final long serialVersionUID = 1L;
     private final Date version = new Date();
+
+    @Override
+    public int describeContents() {
+        return hashCode();
+    }
 
     public Date getVersion() {
         return version;
+    }
+
+    public Serializable readFromParcel(final Parcel parcel) {
+        return parcel.readSerializable();
+    }
+
+    @Override
+    public void writeToParcel(final Parcel parcel, final int flags) {
+        parcel.writeSerializable(this);
     }
 }
